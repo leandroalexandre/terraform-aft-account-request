b7d7ea555aad45d53418b21cf716bee584d46705
leandroalexandre <leandro.alexandre@gmail.com>
2022-07-08 11:44:20 -0300

    create account lab-01

---
+++ terraform/account-lab-01.tf
@@ -0,0 +1,28 @@
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "danilommonteiro+lab-01@outlook.com"
+    AccountName               = "account-lab-01"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "danilommonteiro+aft@outlook.com"
+    SSOUserFirstName          = "Account"
+    SSOUserLastName           = "Lab-01"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "E-mail" = AccountEmail
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Security Automation"
+    change_reason       = "AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "account-lab"
+}
